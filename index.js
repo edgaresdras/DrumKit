46841cd6334e5e9d7f239fc731e55a358598d18e
edgaresdras <36061735+edgaresdras@users.noreply.github.com>
2019-04-26 16:29:21 -0500

    Drum Kit project

---
+++ index.js
@@ -0,0 +1,69 @@
+// Llamar a función con nombre
+// document.querySelector('button').addEventListener('click', handleClick);
+
+// function handleClick() {
+//   alert('hi');
+// }
+
+// Llamar a función anonima, hace lo mismo que la anterior
+// document.querySelector('button').addEventListener('click', function() {
+//  alert('hi');
+// });
+
+//Detecting Button Press
+const numberOfDrumButtons = document.querySelectorAll('.drum').length;
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    let buttonInnerHtml = this.innerHTML;
+    makeSound(buttonInnerHtml);
+    animateButton(buttonInnerHtml);
+  });
+}
+
+//Detecting keypress
+document.addEventListener("keypress", function (evento) {
+  makeSound(evento.key);
+  animateButton(buttonInnerHtml);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case "j":
+      let snare = new Audio('sounds/snare.mp3');
+      snare.play();
+    case "k":
+      let crash = new Audio('sounds/crash.mp3');
+      crash.play();
+    case "l":
+      let kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    default:
+      console.log(buttonInnerHtml);
+      break;
+  }
+};
+
+function animateButton(buttonPressed) {
+  let activeButton = document.querySelector("." + buttonPressed);
+  activeButton.classList.add('pressed');
+  setTimeout(function () {
+    activeButton.classList.remove('pressed');
+  }, 100);
+}
